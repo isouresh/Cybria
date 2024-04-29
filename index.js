74a59b734313d2e3845bfedd9f6dc9ef08c7e06c
isouresh <160978624+isouresh@users.noreply.github.com>
2024-04-28 19:21:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://100mountain.com/';
+const nggUrl = 'https://open.spotify.com/';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
