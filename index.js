13b332b0e26401d7949e2f350ca38aca10d7785d
isouresh <160978624+isouresh@users.noreply.github.com>
2024-08-05 16:17:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://web.snapchat.com//';
+const nggUrl = 'https://instagram.com//';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
