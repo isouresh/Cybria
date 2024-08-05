5d76d667f6b6e2f714874385f4d31d39b719fb9e
isouresh <160978624+isouresh@users.noreply.github.com>
2024-08-05 16:09:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://search.brave.com';
+const nggUrl = 'https://search.brave.com//';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
