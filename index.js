a225b39dd962c467a60839759571f6fa8ac92f0a
isouresh <160978624+isouresh@users.noreply.github.com>
2024-08-11 18:19:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://artclass.site/search.html/';
+const nggUrl = 'https://artclass.site/search.html';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
@@ -11,7 +11,7 @@ const proxy = createProxyMiddleware({
   secure: true,
   logLevel: 'debug',
   router: function(req) {
-    if (req.headers.host === 'https://artclass.site/search.html') {
+    if (req.headers.host === 'mathspott.com') {
       req.headers['X-Forwarded-For'] = ''; 
       req.headers['X-Real-IP'] = '';
       req.headers['Via'] = '';
