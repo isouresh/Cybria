1e7184b30bc40b049c88356f0eea0b5603701b0b
isouresh <160978624+isouresh@users.noreply.github.com>
2024-08-12 10:54:12 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const proxy = createProxyMiddleware({
   secure: true,
   logLevel: 'debug',
   router: function(req) {
-    if (req.headers.host === 'accounts.spotify.com') {
+    if (req.headers.host === 'holyunblocker.org') {
       req.headers['X-Forwarded-For'] = ''; 
       req.headers['X-Real-IP'] = '';
       req.headers['Via'] = '';
