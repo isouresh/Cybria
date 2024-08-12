54f5aa985d23bff058506eb1941e358fb187a213
isouresh <160978624+isouresh@users.noreply.github.com>
2024-08-11 22:18:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const proxy = createProxyMiddleware({
   secure: true,
   logLevel: 'debug',
   router: function(req) {
-    if (req.headers.host === accounts.spotify.com') {
+    if (req.headers.host === 'accounts.spotify.com') {
       req.headers['X-Forwarded-For'] = ''; 
       req.headers['X-Real-IP'] = '';
       req.headers['Via'] = '';
