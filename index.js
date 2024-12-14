4d79fd99dd9b68054c97b131b1f49a96156988be
isouresh <160978624+isouresh@users.noreply.github.com>
2024-12-13 19:50:32 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://google.com/';
+const nggUrl = 'https://play.geforcenow/';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
