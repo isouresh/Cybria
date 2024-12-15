722f7f21265e2a968319882359a967930b5d5750
isouresh <160978624+isouresh@users.noreply.github.com>
2024-12-15 14:13:11 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://holyunblocker.org/';
+const nggUrl = 'https://glacier.fly.dev/';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
@@ -11,7 +11,7 @@ const proxy = createProxyMiddleware({
   secure: true,
   logLevel: 'debug',
   router: function(req) {
-    if (req.headers.host === 'holyunblocker.org') {
+    if (req.headers.host === 'glacier.fly.dev') {
       req.headers['X-Forwarded-For'] = ''; 
       req.headers['X-Real_IP'] = '';
       req.headers['Via'] = '';
