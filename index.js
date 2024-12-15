87ca9d036f8cd80fc9a143e65ac46607540e58ee
isouresh <160978624+isouresh@users.noreply.github.com>
2024-12-14 23:14:08 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://accounts.spotify.com/';
+const nggUrl = 'https://accounts.spotify.com/''https://open.spotify.com/';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
